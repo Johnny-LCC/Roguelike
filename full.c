8f5a296fbfa321fc1254e1afc2b39b29fdb8622f
Johnny-LCC <132676210+Johnny-LCC@users.noreply.github.com>
2023-05-07 12:12:27 +0100

    Update full.c
    Só alterei algumas questões estéticas e adicionei alguns apontadores...

--- full.c
+++ full.c
@@ -3,12 +3,6 @@
 #include <math.h>
 #include <time.h>
 
-/*struct Ponto{
-  int y;
-  int x;
-};
-struct Ponto saida;*/
-
 struct Casas{
   int parede;
   int ocupado;
@@ -16,7 +10,6 @@ struct Casas{
   int saida;
   int cor;
   char c;
-  //struct Ponto cel;
 };
 struct Casas bp[128][256];
 
@@ -30,19 +23,18 @@ struct Player{
   int xp_atual;
   int py;
   int px;
-  //struct Ponto pos;
   char c;
 };
 struct Player j={1, 100, 100, 20, 20, 10, 0, 0, 0, '@'};
 
 int andar;
 
-void menu(int l, int c){
-  int y=l/2, x=c/2;
-  if(c<70){
+void menu(int *l, int *c){
+  int y=(*l)/2, x=(*c)/2;
+  if((*c)<70){
     mvprintw(y-2,x-7,"THE ASCENSION");
   }
-  else if(c<110){
+  else if((*c)<110){
     mvprintw(y-8,x-2, "THE");
     mvprintw(y-7,x-35, ".o888o. .o88888 .o88888 .o88888 88.     8 .o88888 8 .o888o. 88.     8");
     mvprintw(y-6,x-35, "8     8 8       8       8       8*8.    8 8       8 8     8 8*8.    8");
@@ -70,33 +62,34 @@ void menu(int l, int c){
   mvprintw(y+6,x-7,"3 - CREDITOS");
 }
 
-int frame(int l, int c){
-  for(int i=0; i<c; i++){
-    bp[0][i].parede=1; bp[l-1][i].parede=1;
-    bp[0][i].ocupado=0; bp[l-1][i].ocupado=0;
-    bp[0][i].c=' '; bp[l-1][i].c=' ';
-    bp[0][i].cor=1; bp[l-1][i].cor=1;
-    bp[1][i].parede=1; bp[l-2][i].parede=1;
-    bp[1][i].ocupado=0; bp[l-2][i].ocupado=0;
-    bp[1][i].c='#'; bp[l-2][i].c='#';
-    bp[1][i].cor=3; bp[l-2][i].cor=3;
+void frame(int *l, int *c){
+  for(int i=0; i<*c; i++){
+    bp[0][i].parede=1; bp[*l-1][i].parede=1;
+    bp[0][i].ocupado=0; bp[*l-1][i].ocupado=0;
+    bp[0][i].c=' '; bp[*l-1][i].c=' ';
+    bp[0][i].cor=1; bp[*l-1][i].cor=1;
+    
+    bp[1][i].parede=1; bp[*l-2][i].parede=1;
+    bp[1][i].ocupado=0; bp[*l-2][i].ocupado=0;
+    bp[1][i].c='#'; bp[*l-2][i].c='#';
+    bp[1][i].cor=3; bp[*l-2][i].cor=3;
   }
-  for(int i=1; i<l-1; i++){
-    bp[i][0].parede=1; bp[i][c-1].parede=1;
-    bp[i][0].ocupado=0; bp[i][c-1].ocupado=0;
-    bp[i][0].c=' '; bp[i][c-1].c=' ';
-    bp[i][0].cor=1; bp[i][c-1].cor=1;
-    bp[i][1].parede=1; bp[i][c-2].parede=1;
-    bp[i][1].ocupado=0; bp[i][c-2].ocupado=0;
-    bp[i][1].c='#'; bp[i][c-2].c='#';
-    bp[i][1].cor=3; bp[i][c-2].cor=3;
+  for(int i=1; i<*l-1; i++){
+    bp[i][0].parede=1; bp[i][*c-1].parede=1;
+    bp[i][0].ocupado=0; bp[i][*c-1].ocupado=0;
+    bp[i][0].c=' '; bp[i][*c-1].c=' ';
+    bp[i][0].cor=1; bp[i][*c-1].cor=1;
+    
+    bp[i][1].parede=1; bp[i][*c-2].parede=1;
+    bp[i][1].ocupado=0; bp[i][*c-2].ocupado=0;
+    bp[i][1].c='#'; bp[i][*c-2].c='#';
+    bp[i][1].cor=3; bp[i][*c-2].cor=3;
   }
-  return 0;
 }
 
-int grid3x3(int l, int c, int x){
-  for(int i=2; i<l-2; i++){
-    for(int j=2; j<c-2; j++){
+void grid3x3(int *l, int *c, int x){
+  for(int i=2; i<*l-2; i++){
+    for(int j=2; j<*c-2; j++){
       int c=0;
       if(bp[i-1][j].parede==1) c++;
       else if(bp[i-1][j+1].parede==1) c++;
@@ -120,28 +113,26 @@ int grid3x3(int l, int c, int x){
       }
     }
   }
-  return 0;
 }
 
-int saida(int l, int c){
+void saida(int *l, int *c){
   srand(time(NULL));
   int sy, sx;
   do{
-   sy = rand() % l+1;
-   sx = rand() % c+1;
+   sy = rand() % (*l)+1;
+   sx = rand() % (*c)+1;
   }while(bp[sy][sx].parede==1);
   bp[sy][sx].c='o';
   bp[sy][sx].saida=1;
   bp[sy][sx].cor=6;
-  return 0;
 }
 
-int mapa(int l, int c){
+void mapa(int *l, int *c){
   srand(time(NULL));
   double n;
   frame(l,c);
-  for(int i=2; i<l-2; i++){
-    for(int j=2; j<c-2; j++){
+  for(int i=2; i<*l-2; i++){
+    for(int j=2; j<*c-2; j++){
         n= drand48();
         if(n<0.27500){
           bp[i][j].parede=1;
@@ -161,24 +152,23 @@ int mapa(int l, int c){
   grid3x3(l,c,2);
   saida(l,c);  
   do{
-   j.py = rand() % l+1;
-   j.px = rand() % c+1;
+   j.py = rand() % *l+1;
+   j.px = rand() % *c+1;
   }while(bp[j.py][j.px].parede==1 || bp[j.py][j.px].c=='o');
-  return 0;
 }
 
-int action(int t){
+int action(int *t){
   int r=0;
   mvaddch(j.py, j.px, ' ');
-  if(t==27) andar+=100;
-  else if (t==56 && bp[j.py-1][j.px].parede==0) j.py--;
-  else if (t==50 && bp[j.py+1][j.px].parede==0) j.py++;
-  else if (t==52 && bp[j.py][j.px-1].parede==0) j.px--;
-  else if (t==54 && bp[j.py][j.px+1].parede==0) j.px++;
-  else if (t==55 && bp[j.py-1][j.px-1].parede==0) {j.py--; j.px--;}
-  else if (t==57 && bp[j.py-1][j.px+1].parede==0) {j.py--; j.px++;}
-  else if (t==49 && bp[j.py+1][j.px-1].parede==0) {j.py++; j.px--;}
-  else if (t==51 && bp[j.py+1][j.px+1].parede==0) {j.py++; j.px++;}
+  if(*t==27) andar+=100;
+  else if (*t==56 && bp[j.py-1][j.px].parede==0) j.py--;
+  else if (*t==50 && bp[j.py+1][j.px].parede==0) j.py++;
+  else if (*t==52 && bp[j.py][j.px-1].parede==0) j.px--;
+  else if (*t==54 && bp[j.py][j.px+1].parede==0) j.px++;
+  else if (*t==55 && bp[j.py-1][j.px-1].parede==0) {j.py--; j.px--;}
+  else if (*t==57 && bp[j.py-1][j.px+1].parede==0) {j.py--; j.px++;}
+  else if (*t==49 && bp[j.py+1][j.px-1].parede==0) {j.py++; j.px--;}
+  else if (*t==51 && bp[j.py+1][j.px+1].parede==0) {j.py++; j.px++;}
   mvaddch(j.py, j.px, j.c);
   if(bp[j.py][j.px].saida==1) r=1;
   return r;
@@ -202,14 +192,14 @@ int main(){
   getmaxyx(stdscr, l,c);
   do{
     clear();
-    menu(l,c);
+    menu((&l), (&c));
     t=getch();
-    clear();
     switch(t){
       case 49:
+        clear();
         while (andar<100){
           aux=andar;
-          mapa(l,c);
+          mapa(&l, &c);
           for(int i=0; i<l; i++){
             for(int j=0; j<c; j++){
               attron(COLOR_PAIR(bp[i][j].cor));
@@ -218,32 +208,36 @@ int main(){
             }
           }
           mvprintw(0,1, "level %d     hp:%d/%d     mp:%d/%d", j.level, j.hp_atual, j.hp_max, j.mp_atual, j.mp_max);
-          mvprintw(0, c-10, "Floor %d", andar);
+          mvprintw(l-1, c-10, "Floor %d", andar);
           mvaddch(j.py, j.px, j.c);
           do{
             a=getch();
-            aux+=action(a);
+            aux+=action(&a);
           } while (aux==andar);
           andar++;
         }
         clear();
-        printw("FIM... PARABENS");
-        getch();
+        andar=1;
+        printw("\t\t\tFIM... PARABENS\n\n\tPrima 'ESC' para sair do jogo ou qualquer outra tecla para voltar ao menu.");
+        t=getch();
         break;
       case 50:
-        printw("Utilize o keypad para movimentar/atacar e pressione 'i' para abrir o inventário.");
+        clear();
+        printw("\tUtilize o keypad para movimentar/atacar e pressione 'i' para abrir o inventário.");
         getch();
         break;
       case 51:
-        printw("JOAO\nALEXIS\nPATRICIA\nANA\nLCC-LAI");
+        clear();
+        printw("\tJOAO\n\tALEXIS\n\tPATRICIA\n\tANA\n\tLCC-LA1");
         getch();
         break;
       case 27:
-        printw("TEM CERTEZA QUE DESEJA SAIR DO JOGO? (prima 'esc' novamente para sair)");
+        clear();
+        printw("\tTEM CERTEZA QUE DESEJA SAIR DO JOGO? (prima 'esc' novamente para sair)");
         t=getch();
         break;
       default:
-        printw("Prima '1', '2', '3' ou 'esc'.");
+        mvprintw(l-1, (c/2)-15, "Prima '1', '2', '3' ou 'ESC'.");
         getch();
         break;
     }
