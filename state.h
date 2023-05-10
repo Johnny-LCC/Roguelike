2bf17f4882b777643a3c603c6e263a3a53e56607
Johnny-LCC <132676210+Johnny-LCC@users.noreply.github.com>
2023-05-10 18:56:42 +0100

    Update and rename state_prof.h to state.h

---
+++ state.h
@@ -0,0 +1,38 @@
+#ifndef ___STATE_H___
+#define ___STATE_H___
+struct Casas{
+  int parede;
+  int ocupado;
+  int visivel;
+  int saida;
+  int cor;
+  char c;
+};
+
+struct Player{
+  int level;
+  int hp_max;
+  int hp_atual;
+  int mp_max;
+  int mp_atual;
+  int xp_max;
+  int xp_atual;
+  int py;
+  int px;
+  char c;
+};
+
+struct Mobs{
+  int n;
+  int py;
+  int px;
+  char tipo[20];
+};
+
+struct state{
+  struct Casas bp[128][256];
+  struct Player j;
+  struct Mobs monstro[10];
+  int andar;
+};
+#endif
