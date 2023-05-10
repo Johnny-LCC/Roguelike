477676b1a3ed80f4c812312f41364b15082a6aba
Johnny-LCC <132676210+Johnny-LCC@users.noreply.github.com>
2023-05-10 18:54:59 +0100

    Create action.h

---
+++ action.h
@@ -0,0 +1,10 @@
+#ifndef ___ACTION_H___
+#define ___ACTION_H___
+
+#include "state.h"
+
+void update_visibility(int *l, int *c, struct state *s);
+void mmovement_alt(struct state *s);
+void action(int *t, int *l, int *c, struct state *s);
+
+#endif
