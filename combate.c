8e89040ba211e0611ed31d6b2a08c2917b3ef40e
Patrícia Bastos <116760511+patdfb@users.noreply.github.com>
2023-05-15 12:42:02 +0100

    Update combate.c
    Tem erros: necessário colocar as coordenadas (?) nos mvprintw, o j.hp_atual não está a funcionar, fazer mudanças dependendo do que escolhemos

--- combate.c
+++ combate.c
@@ -12,38 +12,72 @@
 
 //como começar, como usar o hp atual do player decidido noutros sitios para aqui (?)
 
-void combate(int *l, int *c, int j.hp_atual/*, int hp_inimigo*/){  //colocar hp aqui ????, falta hp do inimigo
+void combate(int *l, int *c, int player.hp_atual/*, int hp_inimigo*/){  //colocar hp aqui ????, falta hp do inimigo
     
     int hp_inimigo = 40;
-    int ataqueJogador = 5; 
+    int ataqueJogador = 5; //colocar itens do inventário
     int ataqueInimigo = 5; //trocar
 
     int heal = 5; //poções (?)
 
-    srand(time(NULL));
-    int r = rand();  //???? aleatorio
+    srand(time(NULL)); //time(NULL) = tempo neste momento (inicializa o rand)(srand() is used to initialise random number generators. This function gives a starting point for producing the pseudo-random integer series. The argument is passed as a seed for generating a pseudo-random number.)
+    int r = rand() % 2;  //pega um numero aleatório, divide por dois para ver o resto, dando 0 ou 1
 
-    while(j.hp_atual>0 && hp_inimigo>0){
+    clear();
+
+            
+mvprintw("            ,'-',");
+mvprintw("          :-----:");
+mvprintw("      (''' , - , ''')");
+mvprintw("      \   ' .  , `  /");
+mvprintw("       \  '   ^  ? /");
+mvprintw("        \ `   -  ,'");
+mvprintw("         `j_ _,'");
+mvprintw("    ,- -`\ \  /f");
+mvprintw("  ,-      \_\/_/'-");
+mvprintw(" ,                 `,");
+mvprintw(" ,                   ,");
+mvprintw("      /\          \ ");
+mvprintw("|    /             \   ',");
+mvprintw(",   f  :           :`,  ,");
+mvprintw("<...\  ,           : ,- '");
+mvprintw("\,,,,\ ;           : j  '");
+mvprintw(" \    \            :/^^^^'");
+mvprintw("  \    \            ; ''':");
+mvprintw("    \   -,         -`.../");
+mvprintw("     '    - -,`,--`");
+mvprintw("      \_._'-- '---:");
+
+mvprintw("HP do Jogador : ""%d", player.hp_atual"");
+
+mvprintw("        _____");
+mvprintw("    .-,;='';_),-.");
+mvprintw("     \_\(),()/_/");
+mvprintw("       (,___,)");
+mvprintw("      ,-/`~`\-,___");
+mvprintw("     / /).:.('--._)");
+mvprintw("    {_[ (_,_)");
+mvprintw("        | Y |");
+mvprintw("      /  |  \ ");
+mvprintw("      """ """ ");
+
+
+    while(player.hp_atual>0 && hp_inimigo>0){
 
         //turno do jogador
         mvprintw("-- Turno do Jogador --");
-        mvprintw("HP do Jogador : ""%d", j.hp_atual"");
         mvprintw("HP do Inimigo: ""%d", hp_inimigo"");
         mvprintw("a - Ataque");
         mvprintw("h - Curar");
-        mvprintw("r - Fugir");
 
-        char escolha[] = mvscanw();
-
-        if(escolha == "a"){
+        case 97:
             hp_inimigo -= ataqueJogador;
             mvprintw("O Jogador atacou o inimigo e deu ""%d", ataqueJogador" de dano.");
-        } 
-        else if(escolha == "h"){
-            j.hpinicial += heal;
+        
+        case 104:
+            player.hpinicial += heal;
             mvprintw("O jogador recebeu ""%d", heal" pontos de cura.");
-        }
-        else break;
+        
 
         //turno do inimigo
         if(hp_inimigo > 0){
@@ -51,7 +85,7 @@ void combate(int *l, int *c, int j.hp_atual/*, int hp_inimigo*/){  //colocar hp 
             int escolhaInimigo = r;
 
             if(escolhaInimigo == 0){
-                j.hp_atual -= ataqueInimigo;
+                player.hp_atual -= ataqueInimigo;
             mvprintw("O inimigo atacou o Jogador e deu ""%d", ataqueInimigo" de dano.");
             } 
             else {
@@ -60,13 +94,17 @@ void combate(int *l, int *c, int j.hp_atual/*, int hp_inimigo*/){  //colocar hp 
             }
         }
 
-        if(j.hp_atual == 0){
+        if(player.hp_atual == 0){
             mvprintw("GAME OVER");
-            mvprintw("espaço para voltar");
-            //fazer voltar ao menu inicial 
+            mvprintw("Clique em qualquer tecla para voltar");
+            getch();
+            clear(); 
+            menu((&l), (&c));
         }
         if(hp_inimigo == 0){
-            mvprintw("YOU WIN");
+            mvprintw("VITÓRIA");
+            mvprintw("Clique em qualquer tecla para continuar");
+            getch();
             //fazer continuar
         }
     }
