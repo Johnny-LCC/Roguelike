6ac2dc0778e8a79b00fa23d7be1b21988258ef78
Johnny-LCC <132676210+Johnny-LCC@users.noreply.github.com>
2023-05-15 21:47:55 +0100

    Update state.h

--- state.h
+++ state.h
@@ -22,8 +22,22 @@ struct Player{
   char c;
 };
 
+struct Item{
+  char nome[20];
+  int consumivel;
+  int quantidade;
+  int equipavel;
+  int atk;
+  int def;
+  int stats;
+};
+
+struct Inventario{
+  struct Item item;
+  struct Inventario *next;
+};
+
 struct Mobs{
-  int n;
   int py;
   int px;
   char tipo[20];
@@ -32,7 +46,10 @@ struct Mobs{
 struct state{
   struct Casas **bp;
   struct Player j;
+  struct Inventario *inventario;
   struct Mobs monstro[10];
   int andar;
+  int l;
+  int c;
 };
 #endif
