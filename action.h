46f985ae82febb34d1a748080461c7c995be20e1
Johnny-LCC <132676210+Johnny-LCC@users.noreply.github.com>
2023-05-15 21:47:00 +0100

    Update action.h

--- action.h
+++ action.h
@@ -5,6 +5,8 @@
 
 void update_visibility(int *l, int *c, struct state *s);
 void mmovement_alt(struct state *s);
+void listar(struct state *s);
+void combate(struct state *s);
 void action(int *t, int *l, int *c, struct state *s);
 
 #endif
