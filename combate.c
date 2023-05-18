d400ea9648447aba554ee646c9509a3bec4b4214
Patrícia Bastos <116760511+patdfb@users.noreply.github.com>
2023-05-18 10:50:34 +0100

    Update combate.c

--- combate.c
+++ combate.c
@@ -12,13 +12,14 @@
 
 //como começar, como usar o hp atual do player decidido noutros sitios para aqui (?)
 
-void combate(int *l, int *c, int player.hp_atual/*, int hp_inimigo*/){  //colocar hp aqui ????, falta hp do inimigo
+void combate(struct state *s){  //colocar hp aqui ????, falta hp do inimigo
     
-    int hp_inimigo = 40;
-    int ataqueJogador = 5; //colocar itens do inventário
-    int ataqueInimigo = 5; //trocar
+    int ataqueJogador =  ; 
+    int ataque_monstro = s->monstro[i].atk ; //trocar
 
     int heal = 5; //poções (?)
+
+    char k;
 
     srand(time(NULL)); //time(NULL) = tempo neste momento (inicializa o rand)(srand() is used to initialise random number generators. This function gives a starting point for producing the pseudo-random integer series. The argument is passed as a seed for generating a pseudo-random number.)
     int r = rand() % 2;  //pega um numero aleatório, divide por dois para ver o resto, dando 0 ou 1
@@ -48,7 +49,7 @@ mvprintw("    \   -,         -`.../");
 mvprintw("     '    - -,`,--`");
 mvprintw("      \_._'-- '---:");
 
-mvprintw("HP do Jogador : ""%d", player.hp_atual"");
+mvprintw("HP do Jogador : ""%d", s->j.hp_atual"");
 
 mvprintw("        _____");
 mvprintw("    .-,;='';_),-.");
@@ -62,46 +63,49 @@ mvprintw("      /  |  \ ");
 mvprintw("      """ """ ");
 
 
-    while(player.hp_atual>0 && hp_inimigo>0){
+    while(s->j.hp_atual>0 && s->monstro[i].hp_atual >0){
 
         //turno do jogador
         mvprintw("-- Turno do Jogador --");
-        mvprintw("HP do Inimigo: ""%d", hp_inimigo"");
+        mvprintw("HP do Inimigo: ""%d", s->monstro[i].hp_atual "");
         mvprintw("a - Ataque");
         mvprintw("h - Curar");
 
-        case 97:
-            hp_inimigo -= ataqueJogador;
-            mvprintw("O Jogador atacou o inimigo e deu ""%d", ataqueJogador" de dano.");
-        
-        case 104:
-            player.hpinicial += heal;
-            mvprintw("O jogador recebeu ""%d", heal" pontos de cura.");
-        
+        scanw("%c", k);
 
+        switch(k){
+            case 97:
+                s->monstro[i].hp_atual  -= ataqueJogador;
+                mvprintw("O Jogador atacou o inimigo e deu ""%d", ataqueJogador" de dano.");
+            
+            case 104:
+                s->j.hp_atual += heal;
+                mvprintw("O jogador recebeu ""%d", heal" pontos de cura.");
+            
+        }
         //turno do inimigo
-        if(hp_inimigo > 0){
+        if(s->monstro[i].hp_atual  > 0){
             mvprintw("-- Turno do Inimigo --");
             int escolhaInimigo = r;
 
             if(escolhaInimigo == 0){
-                player.hp_atual -= ataqueInimigo;
-            mvprintw("O inimigo atacou o Jogador e deu ""%d", ataqueInimigo" de dano.");
+                s->j.hp_atual -= ataque_monstro;
+            mvprintw("O inimigo atacou o Jogador e deu ""%d", ataque_monstro" de dano.");
             } 
             else {
-                hp_inimigo += heal;
+                s->monstro[i].hp_atual  += heal;
                 mvprintw("O inimigo recebeu ""%d", heal" pontos de cura.");
             }
         }
 
-        if(player.hp_atual == 0){
+        if(s->j.hp_atual == 0){
             mvprintw("GAME OVER");
             mvprintw("Clique em qualquer tecla para voltar");
             getch();
             clear(); 
-            menu((&l), (&c));
+            menu((&s->l), (&s->c));
         }
-        if(hp_inimigo == 0){
+        if(s->monstro[i].hp_atual  == 0){
             mvprintw("VITÓRIA");
             mvprintw("Clique em qualquer tecla para continuar");
             getch();
