13bcf6b12a80b1ee65c44ae492d4ca8c0378bfe7
Johnny-LCC <132676210+Johnny-LCC@users.noreply.github.com>
2023-05-18 10:47:38 +0100

    Update mapa.c

--- mapa.c
+++ mapa.c
@@ -102,7 +102,7 @@ void generate_map(struct state *s){
      s->mobs[i].py = rand() % s->l;
      s->mobs[i].px = rand() % s->c;
     }while(s->bp[s->mobs[i].py][s->mobs[i].px].parede==1 || s->bp[s->mobs[i].py][s->mobs[i].px].saida==1 || s->bp[s->mobs[i].py][s->mobs[i].px].c=='@');
-    s->bp[s->mobs[i].py][s->mobs[i].px].c='X'; s->bp[s->mobs[i].py][s->moba[i].px].cor=5; s->bp[s->mobs[i].py][s->mobs[i].px].ocupado=1; 
+    s->bp[s->mobs[i].py][s->mobs[i].px].c='X'; s->bp[s->mobs[i].py][s->mobs[i].px].cor=5; s->bp[s->mobs[i].py][s->mobs[i].px].ocupado=1; 
   }
 }
 
