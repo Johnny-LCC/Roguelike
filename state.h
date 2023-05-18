30ad9dc273a241e445c854745858bf4dfd1eddf0
Johnny-LCC <132676210+Johnny-LCC@users.noreply.github.com>
2023-05-18 09:54:25 +0100

    Update state.h

--- state.h
+++ state.h
@@ -5,6 +5,7 @@ struct Casas{
   int ocupado;
   int visivel;
   int saida;
+  int drop;
   int cor;
   char c;
 };
@@ -13,23 +14,29 @@ struct Player{
   int level;
   int hp_max;
   int hp_atual;
-  int mp_max;
-  int mp_atual;
   int xp_max;
   int xp_atual;
+  int atk;
+  int def;
   int py;
   int px;
   char c;
 };
+
+struct Dano{
+  int n_dados;
+  int n_faces;
+}
 
 struct Item{
   char nome[20];
   int consumivel;
   int quantidade;
   int equipavel;
-  int atk;
-  int def;
-  int stats;
+  int equipado;
+  struct Dano dmg;
+  int arm;
+  int hp;
 };
 
 struct Inventario{
@@ -37,19 +44,32 @@ struct Inventario{
   struct Inventario *next;
 };
 
+struct Monstro{
+  int hp_max;
+  int hp_atual;
+  int atk;
+  int def;
+  struct Dano dmg;
+  int arm;
+  char tipo[20];
+};
+
 struct Mobs{
   int py;
   int px;
-  char tipo[20];
+  struct Monstro inimigo;
 };
 
 struct state{
   struct Casas **bp;
   struct Player j;
+  struct Item drops[5];
   struct Inventario *inventario;
-  struct Mobs monstro[10];
+  struct Monstro monstro[10];
+  struct Mobs mobs[10];
   int andar;
   int l;
   int c;
+  
 };
 #endif
