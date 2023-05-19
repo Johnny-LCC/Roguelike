ad39e0986bb1d36f8a1d266db127df96f5477534
Johnny-LCC <132676210+Johnny-LCC@users.noreply.github.com>
2023-05-19 12:06:49 +0100

    Update action.h

--- action.h
+++ action.h
@@ -3,8 +3,11 @@
 
 #include "state.h"
 
+int dice(int num, int faces);
 void update_visibility(struct state *s);
+void mcombate(struct state *s, int index);
 void maction(struct state *s);
+void combate(struct state *s, int y, int x);
 void action(int *t, struct state *s);
 
 #endif
